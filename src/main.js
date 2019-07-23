7bbdcaa3873583b54f5092e7094274043481f5eb
Gaëtan Dezeiraud <gaetan.dezeiraud@outlook.com>
2019-07-23 18:52:43 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const {app, BrowserWindow, shell} = require('electron');
+const { autoUpdater } = require('electron-updater');
+const path = require('path');
+
+// Global reference to mainWindow
+let mainWindow;
+
+function createMainWindow() {
+    let window = new BrowserWindow({
+        width: 1080,
+        height: 720,
+        frame: false,
+		icon: path.join(__dirname, 'assets/icon.png'),
+        webPreferences: {
+			webviewTag: true,
+            nodeIntegration: true
+        }
+    });
+    
+    window.setMenu(null);
+    window.loadFile(path.join(__dirname, 'renderer/index.html'));
+	
+    // Uncomment to use Chrome developer tools
+    // window.webContents.openDevTools();
+	
+    // Cleanup when window is closed
+    window.on('closed', function() {
+        window = null;
+    });
+
+    return window;
+}
+
+// Setup application - For PiP
+// app.commandLine.appendSwitch('disable-site-isolation-trials');
+
+// Quit application when all windows are closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin')
+        app.quit();
+});
+
+app.on('activate', () => {
+    // On macOS it is common to re-create a window even after all windows have been closed
+    if (mainWindow === null)
+        mainWindow = createMainWindow();
+});
+
+// Create main BrowserWindow when electron is ready
+app.on('ready', () => {
+	autoUpdater.checkForUpdatesAndNotify();
+    mainWindow = createMainWindow();
+});
+
+// Auto Update
+autoUpdater.on('update-downloaded', info => {
+	autoUpdater.quitAndInstall();
+});
---
+++ src/renderer/index.js
@@ -0,0 +1,85 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const {shell, remote} = require('electron');
+
+function reloadIframe() {
+	document.getElementById('window-iframe').contentDocument.location.reload(true);
+	console.log("call");
+}
+
+(function handleWindowControls() {
+    // When document has loaded, initialise
+    document.onreadystatechange = () => {
+        if (document.readyState == "complete") {
+            init();
+        }
+    };
+
+    function init() {
+        let window = remote.getCurrentWindow();
+        const iframe = document.getElementById('window-iframe'),
+			pictureInPictureButton = document.getElementById('picture-in-picture-button'),
+			reloadButton = document.getElementById('reload-button'),
+			minButton = document.getElementById('min-button'),
+            maxButton = document.getElementById('max-button'),
+            restoreButton = document.getElementById('restore-button'),
+            closeButton = document.getElementById('close-button');
+
+		/*pictureInPictureButton.addEventListener("click", event => {
+			// Picture in Picture is not available in Electron (requestPictureInPicture) - See https://github.com/electron/electron/pull/17686
+			// So create fake PiP feature
+        });*/
+		
+		// For Debug only
+		// iframe.webContents.openDevTools();
+		
+		iframe.addEventListener('new-window', function(e) {
+			shell.openExternal(e.url);
+		});
+
+		reloadButton.addEventListener("click", event => {
+            iframe.reload();
+        });
+
+        minButton.addEventListener("click", event => {
+            window = remote.getCurrentWindow();
+            window.minimize();
+        });
+
+        maxButton.addEventListener("click", event => {
+            window = remote.getCurrentWindow();
+            window.maximize();
+            toggleMaxRestoreButtons();
+        });
+
+        restoreButton.addEventListener("click", event => {
+            window = remote.getCurrentWindow();
+            window.unmaximize();
+            toggleMaxRestoreButtons();
+        });
+
+        // Toggle maximise/restore buttons when maximisation/unmaximisation
+        // occurs by means other than button clicks e.g. double-clicking
+        // the title bar:
+        toggleMaxRestoreButtons();
+        window.on('maximize', toggleMaxRestoreButtons);
+        window.on('unmaximize', toggleMaxRestoreButtons);
+
+        closeButton.addEventListener("click", event => {
+            window = remote.getCurrentWindow();
+            window.close();
+        });
+
+        function toggleMaxRestoreButtons() {
+            window = remote.getCurrentWindow();
+            if (window.isMaximized()) {
+                maxButton.style.display = "none";
+                restoreButton.style.display = "flex";
+            } else {
+                restoreButton.style.display = "none";
+                maxButton.style.display = "flex";
+            }
+        }
+    }
+})();
